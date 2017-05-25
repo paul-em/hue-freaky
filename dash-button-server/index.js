d24acdf0bdc852f9f3ce1ec8d711965dd4e12a71
Paul Em <paul3m@gmail.com>
2017-05-25 23:36:20 +0200

    work on dash buttons

--- dash-button-server/index.js
+++ dash-button-server/index.js
@@ -1,6 +1,28 @@
 const dashButton = require('node-dash-button');
 const config = require('../config');
 
-dashButton(config.dashButtonAddresses).on('detected', (id) => {
-  console.log('press detected!', id, config.dashButtonBrands[config.dashButtonAddresses.indexOf(id)]);
+const dashButtons = [
+  {
+    address: 'ac:63:be:8b:f2:32',
+    brand: 'Somat',
+  },
+  {
+    address: '50:f5:da:42:94:1e',
+    brand: 'Oral-B',
+  },
+  {
+    address: 'ac:63:be:60:be:c4',
+    brand: 'Gillete',
+  },
+  {
+    address: '0c:47:c9:1a:5d:6f',
+    brand: 'Nerf',
+  },
+
+];
+
+
+dashButton(dashButtons.map(button => button.address)).on('detected', (address) => {
+  const button = dashButtons.find(button => button.address === address);
+  console.log('press detected!', address, button.brand);
 });
