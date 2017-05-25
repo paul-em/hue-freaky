25a788c38adbaabb0048a9677b476c10bb65a7ec
Paul Em <paul3m@gmail.com>
2017-05-25 23:47:01 +0200

    work on dash buttons

--- dash-button-server/index.js
+++ dash-button-server/index.js
@@ -1,4 +1,7 @@
+const WebSocket = require('ws');
 const dashButton = require('node-dash-button');
+
+const wss = new WebSocket.Server({ port: 80 });
 
 const dashButtons = [
   {
@@ -17,11 +20,15 @@ const dashButtons = [
     address: '0c:47:c9:1a:5d:6f',
     brand: 'Nerf',
   },
-
 ];
 
 
 dashButton(dashButtons.map(button => button.address), null, null, 'all').on('detected', (address) => {
   const button = dashButtons.find(button => button.address === address);
   console.log('press detected!', address, button.brand);
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(button.brand);
+    }
+  })
 });
