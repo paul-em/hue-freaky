ff349b5c111da0b1fea858a4cae640be8f008bf4
Paul Em <paul3m@gmail.com>
2017-05-29 11:19:21 +0200

    updated ip

--- dash-button-client/index.js
+++ dash-button-client/index.js
@@ -1,6 +1,6 @@
 const WebSocket = require('ws');
 
-const ws = new WebSocket('ws://192.168.1.2');
+const ws = new WebSocket('ws://192.168.1.4');
 
 ws.on('open', () => {
   console.log('connection open');
